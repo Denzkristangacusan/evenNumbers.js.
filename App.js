90a3ddc9d1754bf769086b401cde1a27d537f864
Denzkristangacusan <gdenzkristan@gmail.com>
2024-10-09 23:11:53 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+console.log("All numbers from 1 to 10:");
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
+console.log("Even numbers between 1 and 10:");
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
